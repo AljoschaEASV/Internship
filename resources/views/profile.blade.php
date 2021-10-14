df9afdd9439404978e02466756913f6b4cd523f1
Aljoscha Wiesner <aljoscha.wiesner@visuellverstehen.de>
2021-10-14 16:35:47 +0200

    Implement english profile page

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -3,19 +3,19 @@
         <aside>
             <img class="profile__picture" src="{{ $user->profile_picture }}" alt="">
             <h3 class="title title--xxl">{{ $user->first_name }} {{ $user->last_name }}</h3>
-            <h4 class="title">Straße</h4>
+            <h4 class="title">Street</h4>
             <p class="paragraph">{{ $user->address->street }}</p>
-            <h4 class="title">Stadt</h4>
+            <h4 class="title">City</h4>
             <p class="paragraph">{{ $user->address->city }}</p>
         </aside>
         <section>
             <article class="profile__gardens">
-                <h2 class="title title--xxl title--bold">Deine Gärten</h2>
-                <p class="paragraph">Lege jetzt deinen ersten Garten an!</p>
+                <h2 class="title title--xxl title--bold">Your gardens</h2>
+                <p class="paragraph">Help us grow - Offer a spot in your garden!</p>
             </article>
             <article class="profile__tours">
-                <h2 class="title title--xxl title--bold">Deine Touren </h2>
-                <p class="paragraph">Fange an Touren anzubieten!</p>
+                <h2 class="title title--xxl title--bold">Your tours</h2>
+                <p class="paragraph">Beginn offering tours</p>
             </article>
         </section>
     </div>
